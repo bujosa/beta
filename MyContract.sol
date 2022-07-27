8c787169b6de556a962d228bfeb1d0397ed37cc7
bujosa <davidbujosa@gmail.com>
2022-07-27 19:03:55 -0400

    feat: add example

--- MyContract.sol
+++ MyContract.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    
+    // State variables
+    uint myUint = 1;
+
+    // Local variables
+    function getValue() public pure returns(uint) {
+        uint value = 1;
+        return value;
+    }
 }

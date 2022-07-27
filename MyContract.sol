3444423b875dc13dfad8f542b747534e8a51777b
bujosa <davidbujosa@gmail.com>
2022-07-27 19:09:42 -0400

    feat: add struct example

--- MyContract.sol
+++ MyContract.sol
@@ -3,7 +3,18 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
     // State variables
-    uint myUint = 1;
+    uint public myUint  = 1;
+    uint256 public myUint256 = 1;
+
+    struct MyStruct {
+        uint myUint;
+        string myString;
+    }
+
+    MyStruct public myStruct = MyStruct({
+        myUint: 2,
+        myString: "hello"
+    });
 
     // Local variables
     function getValue() public pure returns(uint) {

c089a054da2201ccceaff993e07d444891981897
bujosa <davidbujosa@gmail.com>
2022-07-31 21:59:29 -0400

    feat: add owner function and constructor

--- Example.sol
+++ Example.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.0;
 contract MyContract {
     uint[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function countEventNumbers() public view returns (uint) {
         uint count = 0;
         for (uint i = 0; i < numbers.length; i++) {
@@ -19,6 +25,10 @@ contract MyContract {
             return true;
         }
         return false;
+    }
+
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
     }
 
 }

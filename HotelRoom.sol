4cc56a8b2fce576ff157db128785dd09ad83d858
bujosa <davidbujosa@gmail.com>
2022-08-07 20:58:20 -0400

    feat: add costs and check enum modifier functions

--- HotelRoom.sol
+++ HotelRoom.sol
@@ -2,13 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract HotelRoom {
+
+    enum Statuses { Vacant, Occupied}
+
+    Statuses currentStatus;
+
     address payable public owner;
 
     constructor() {
+        // Convert msg.sender to an address
         owner = msg.sender;
+        currentStatus = Statuses.Vacant;
     }
 
-    function book(){
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Room is already occupied");
+        _;
+    }
 
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not enough ether provided");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether) {
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
     }
 }

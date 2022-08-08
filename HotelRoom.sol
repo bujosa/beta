4485a0098e5b56b252ee15e8f8f81cba608bedbe
bujosa <davidbujosa@gmail.com>
2022-08-07 20:49:57 -0400

    feat: add constructor and init book function

--- HotelRoom.sol
+++ HotelRoom.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract HotelRoom {
-    
+    address payable public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function book(){
+
+    }
 }

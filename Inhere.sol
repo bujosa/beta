b984f49b40c1b771d3a98b42af04d282e99cd03c
bujosa <davidbujosa@gmail.com>
2022-08-14 17:42:22 -0400

    feat(inhere): add get secret function

--- Inhere.sol
+++ Inhere.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract MyContract {
+    string secret;
+    address owner;
+
+    constructor(string memory _secret) public {
+        secret = _secret;
+        owner = msg.sender;
+    }
+
+    function getSecret() public view returns (string memory) {
+        return secret;
+    }
+}

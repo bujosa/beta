530bd72e49dc20c194a42e53a76a3568e7768f96
bujosa <davidbujosa@gmail.com>
2022-08-20 03:47:45 -0400

    feat: add secret vault in my contract

--- Inhere.sol
+++ Inhere.sol
@@ -12,10 +12,6 @@ contract Ownable {
 
 contract SecretVault {
     string secret;
-}
-
-contract MyContract is Ownable {
-    string secret;
 
     constructor(string memory _secret) public {
         secret = _secret;
@@ -24,5 +20,19 @@ contract MyContract is Ownable {
 
     function getSecret() public view returns (string memory) {
         return secret;
+    }
+}
+
+contract MyContract is Ownable {
+    address secretVault;
+
+    constructor(string memory _secret) public {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        return SecretVault(secretVault).getSecret();
     }
 }
